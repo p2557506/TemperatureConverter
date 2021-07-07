3431fa83f1426451110304e276b06371faeeb507
Isaac Zerououl <77992848+p2557506@users.noreply.github.com>
2021-07-07 13:16:06 +0100

    Create temperature.js

---
+++ temperature.js
@@ -0,0 +1,6 @@
+const kelvin = 293;//Constant temperature 
+const celsius = kelvin - 273;//Constant temperature
+
+let fahrenheit = Math.floor(celsius * (9/5) + 32);//Conversion to Fahrenheit r
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
